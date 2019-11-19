5e54c74b7896e47028c1adc068418bc9dab199dc
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-11-19 20:45:51 +0545

    Adding more Functional code III.

--- src/udacity_kotlinBootcamp_course/AMS.kt
+++ src/udacity_kotlinBootcamp_course/AMS.kt
@@ -1,6 +1,7 @@
 package udacity_kotlinBootcamp_course
 
 import java.util.*
+import java.util.function.Consumer
 
 fun main(args: Array<String>) {
     println("Hello from Kotlin main with args: ${args[0]}")
@@ -9,6 +10,15 @@ fun main(args: Array<String>) {
     expressionPractice()
 
     feedTheFish()
+
+    swim(5)
+    swim(20, "medium")
+    swim(10, speed = "slow")
+
+    println(canAddFish(10.0, listOf(3, 3, 3)))
+    println(canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1, 1, 3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
 }
 
 fun feedTheFish() {
@@ -16,6 +26,28 @@ fun feedTheFish() {
     val food = fishFood(day)
 
     println("Today is $day and the food eats $food")
+
+    shouldChangeWater("Monday")
+    shouldChangeWater("Monday", 28, 60)
+    shouldChangeWater("Monday", dirty = 50) // if skipping default params, provide param names
+}
+
+fun canAddFish(
+    tankSize: Double,
+    currentFish: List<Int>,
+    fishSize: Int = 2,
+    hasDecorations: Boolean = true
+): Boolean {
+    /* var effectiveTankSize: Double = tankSize.toDouble()
+    if (hasDecorations) effectiveTankSize *= 0.8;
+
+    val deductFishSize = { a: Int -> effectiveTankSize -= a } // Kotlin's version of consumer
+    currentFish.forEach(deductFishSize)
+
+    return effectiveTankSize >= fishSize*/
+
+    // OR, more concise
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
 
 fun fishFood(day: String): String {
@@ -27,6 +59,18 @@ fun fishFood(day: String): String {
         "Saturday" -> "lettuce"
         else -> "fish don't eat today"
     }
+}
+
+fun shouldChangeWater(
+    day: String,
+    temperature: Int = 22,
+    dirty: Int = 20
+): Boolean {
+    return true
+}
+
+fun swim(time: Int, speed: String = "fast") {
+    println("The swimming speed is $speed for time of $time")
 }
 
 fun randomDay(): String {
